16b4cdf138e48bd6326ed6bed024b5d2523d0bb4
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-08-20 18:21:43 -0300

    projeto sendo apresentado por props e links proocean adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header/Header.jsx";
-import Sobre from './components/Sobre/Sobre.jsx';
-import Habilidades from './components/Habilidades/Habilidades.jsx';
-import Contato from './components/Contato/Contato.jsx';
-import polipratik from "./components/img/polipratik.png";
-import proOcean from "./components/img/proocean.png";
-import Projetos from './components/Projetos/Projetos.jsx';
+import Sobre from "./components/Sobre/Sobre.jsx";
+import Habilidades from "./components/Habilidades/Habilidades.jsx";
+import Contato from "./components/Contato/Contato.jsx";
+import Projetos from "./components/Projetos/Projetos.jsx";
+import { projetoPk } from "./Data.jsx";
+import { projetoGs } from "./Data.jsx";
+// import { projetoKGV } from "./Data.jsx";
 
 export default function App() {
-
-  const titulo = ["PolipratiK WebSite" , "Projeto ProOcean"]
-  const desc1 = ["Projeto realizado com HTML, CSS, JavaScript" , "Projeto realizado com HTML, CSS, JavaScript e com prototipação realizada no Figma"]
-  const desc2 = ["e com prototipação realizada no Figma" , "Desenvolvido para a realização do trabalho semestral da FIAP"]
-  const imgSite = [polipratik , proOcean]
-  const linkRepositorio = ["https://github.com/luizpaulo73/site-polipratik" , "https://github.com/yunkbaza/Global_Solution_ProOcean"]
-  const linkSite = ["https://polipratik.com.br" , "#"]
-
   return (
     <>
       <Header />
       <main>
-      <Sobre />
-      <Habilidades />
-      <Projetos titulo={titulo[0]} desc1={desc1[0]} desc2={desc2[0]} imgSite={imgSite[0]} linkRepositorio={linkRepositorio[0]} linkSite={linkSite[0]}/>
-      <Projetos titulo={titulo[1]} desc1={desc1[1]} desc2={desc2[1]} imgSite={imgSite[1]} linkRepositorio={linkRepositorio[1]} linkSite={linkSite[1]}/>
+        <Sobre />
+        <Habilidades />
+        <Projetos titulo={projetoPk[0].title}
+                  desc1={projetoPk[0].desc1}
+                  desc2={projetoPk[0].desc2}
+                  imgSite={projetoPk[0].imgSite}
+                  linkRepositorio={projetoPk[0].linkRepositorio}
+                  linkSite={projetoPk[0].linkSite}
+        />
+        <Projetos titulo={projetoGs[0].title}
+                  desc1={projetoGs[0].desc1}
+                  desc2={projetoGs[0].desc2}
+                  imgSite={projetoGs[0].imgSite}
+                  linkRepositorio={projetoGs[0].linkRepositorio}
+                  linkSite={projetoGs[0].linkSite}
+        />
+        {/* <Projetos titulo={projetoKGV[0].title}
+                  desc1={projetoGs[0].desc1}
+                  desc2={projetoGs[0].desc2}
+                  imgSite={projetoGs[0].imgSite}
+                  linkRepositorio={projetoGs[0].linkRepositorio}
+                  linkSite={projetoGs[0].linkSite}
+
+        /> */}
       </main>
       <Contato />
     </>
-  )
-};
+  );
+}
